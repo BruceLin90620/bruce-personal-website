06328dfc048c6572cbf527e59af2b772b2bd24b3
Bruce Lin <151715807+BruceLin90620@users.noreply.github.com>
2024-08-24 23:58:35 +0800

    Fix: fix bugs with hexapod page

--- next.config.js
+++ next.config.js
@@ -36,23 +36,4 @@ const nextConfig = {
   },
 };
 
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
-};
-
 module.exports = nextConfig;
