88c26586ee42e770db3f3521950055a8e94154cd
Bruce Lin <151715807+BruceLin90620@users.noreply.github.com>
2024-09-14 01:12:22 +0800

    Fix: bugs

--- src/components/Sections/EVpi.tsx
+++ src/components/Sections/EVpi.tsx
@@ -27,7 +27,7 @@ const About: FC = memo(() => {
     <Section className="bg-neutral-800 text-white" sectionId={SectionId.None}>
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="py-12">
-          <h1 className="text-4xl font-bold mb-4">Ummanned Vehicle - EVπ</h1>
+          <h1 className="text-4xl font-bold mb-4">Ummanned Vehicle - EVπ </h1>
           <p className="text-xl text-gray-300 mb-8">
           Collaborated with Foxconn on integrating HHEV.OS into EVπ control system, 
     ensuring compatibility with ROS2 navigation and localization while enhancing security. 
