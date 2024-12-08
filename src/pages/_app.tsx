eec7b0d585004a97040dcf0108e95a68c6ed790f
Bruce Lin <151715807+BruceLin90620@users.noreply.github.com>
2024-12-08 16:02:12 +0800

    fix: bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,30 @@
+import type {AppProps} from 'next/app';
+import Script from 'next/script';
+import {memo, useMemo} from 'react';
 import 'tailwindcss/tailwind.css';
+
 import '../globalStyles.scss';
 
-import type {AppProps} from 'next/app';
-import {memo} from 'react';
-import Script from 'next/script';
-
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
+  const scriptContent = useMemo(() => ({
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-T0JTWH2MYJ');
+    `
+  }), []);
+
   return (
     <>
       <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-T0JTWH2MYJ"
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-T0JTWH2MYJ`}
       />
       <Script
+        dangerouslySetInnerHTML={scriptContent}
         id="google-analytics"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-T0JTWH2MYJ');
-          `,
-        }}
       />
       <Component {...pageProps} />
     </>
